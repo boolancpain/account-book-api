606d44041fed5e29536072e9ed2281e1081b83df
DESKTOP-55A28ML\user <boolancpain@gmail.com>
2023-02-17 17:21:59 +0900

    first commit

---
+++ src/main/java/com/fyo/accountbookapi/AccountBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.fyo.accountbookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountBookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fyo/accountbookapi/AccountBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyo.accountbookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
